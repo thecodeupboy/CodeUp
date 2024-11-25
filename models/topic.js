d88532947eb1d345285adbffea3d644d98661fcb
HimanshuChandnani <himanshu.chandnani2004@gmail.com>
2024-11-25 16:01:40 +0530

    changes made by chirag on my laptop

--- models/topic.js
+++ models/topic.js
@@ -1,19 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const TopicSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  lessonId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Lesson',
-    required: true,
-  },
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    videoUrl: {
+        type: String,
+        required: true,
+    },
+    lessonId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Lesson",
+        required: true,
+    },
 });
 
-module.exports = mongoose.model('Topic', TopicSchema);
+module.exports = mongoose.model("Topic", TopicSchema);
