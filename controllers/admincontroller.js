48a3e41fce230769581ca43a0b0b42004b51bce3
thecodeupboy <chirag.codeup@gmail.com>
2025-01-08 12:37:10 +0530

    jhyhj

--- controllers/admincontroller.js
+++ controllers/admincontroller.js
@@ -160,6 +160,7 @@ exports.restoreUserFromArchive = async (req, res) => {
     }
 
     const restoredUser = new userTable({
+      _id: archivedUser._id,
       googleId: archivedUser.googleId,
       name: archivedUser.name,
       email: archivedUser.email,
